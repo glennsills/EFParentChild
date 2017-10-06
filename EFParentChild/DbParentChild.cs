a147a6b4eecbdf65018d9283df68abb941710dc5
Glenn Sills <gbsills@msn.com>
2017-10-06 19:54:09 -0400

    clean up commented code

--- EFParentChild/DbParentChild.cs
+++ EFParentChild/DbParentChild.cs
@@ -15,13 +15,5 @@ namespace EFParentChild
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ParentChildDb;Trusted_Connection=True;");
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            //modelBuilder.Entity<Child>()
-            //    .HasOne(p => p.Parent)
-            //    .WithMany(b => b.Children)
-            //    .HasForeignKey(p => p.ParentId)
-            //    .HasConstraintName("FK_Child_Parent");
-        }
     }
 }
