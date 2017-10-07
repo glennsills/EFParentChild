e16212de2785e433ec11ea768a7999de16c46489
Glenn Sills <gbsills@msn.com>
2017-10-06 20:15:33 -0400

    cleanup

--- EFParentChild/Child.cs
+++ EFParentChild/Child.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace EFParentChild
+﻿namespace EFParentChild
 {
     public class Child
     {
--- EFParentChild/Program.cs
+++ EFParentChild/Program.cs
@@ -11,13 +11,20 @@ namespace EFParentChild
                 var parent = new Parent
                 {
                     FirstName = "Joe",
-                    LastName = "Blow"
+                    LastName = "Smith"
                 };
+
                 parent.Children.Add(
                     new Child
                     {
                         FirstName = "LittleJoe",
-                        LastName = "Blow"
+                        LastName = "Smith"
+                    });
+                parent.Children.Add(
+                    new Child
+                    {
+                        FirstName = "Anne",
+                        LastName = "Smith"
                     });
 
                 dbContext.Add(parent);
